8d7e258f47f95fac31ea9cc1488f94d072dfa29e
fighter23085 <info@eim-systems.org>
2022-04-24 10:33:42 +0200

    fixed StoredActivity

--- app/src/main/java/org/eim_systems/privatetracker/StoredActivity.java
+++ app/src/main/java/org/eim_systems/privatetracker/StoredActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.resources.TextAppearanceFontCallback;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +18,10 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import org.eim_systems.privatetracker.databinding.ActivityStoredBinding;
+import org.w3c.dom.Text;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 public class StoredActivity extends AppCompatActivity {
 
@@ -41,10 +46,28 @@ public class StoredActivity extends AppCompatActivity {
     private void addRows(){
         PersistenceManager.init(getApplicationContext());
         Cursor cursor = PersistenceManager.getInstance().getCursor();
-
-        for(cursor.moveToFirst(); !cursor.isAfterLast();cursor.moveToLast()){
-            ConstraintLayout child = (ConstraintLayout) getLayoutInflater().inflate(R.layout.track, linearLayout);
+        for(cursor.moveToFirst(); !cursor.isAfterLast();cursor.moveToNext()){
+            ConstraintLayout child = (ConstraintLayout) getLayoutInflater().inflate(R.layout.track, null);
             linearLayout.addView(child);
+
+            TextView name = findViewById(R.id.nameField);
+            int nameIndex = cursor.getColumnIndexOrThrow("name");
+            name.setText(cursor.getString(nameIndex));
+            TextView distance = findViewById(R.id.distanceTv);
+            int distanceIndex = cursor.getColumnIndexOrThrow("distance");
+            distance.setText(cursor.getString(distanceIndex));
+            TextView upTv = findViewById(R.id.upTv);
+            int upIndex = cursor.getColumnIndexOrThrow("up");
+            upTv.setText(cursor.getString(upIndex));
+            TextView downTv = findViewById(R.id.downTv);
+            int downIndex = cursor.getColumnIndexOrThrow("down");
+            downTv.setText(cursor.getString(downIndex));
+            TextView dateTv = findViewById(R.id.dateTv);
+            int dateIndex = cursor.getColumnIndexOrThrow("date");
+            long datel = cursor.getLong(dateIndex);
+            Date date = new Date(datel);
+            DateFormat dateFormat = DateFormat.getDateInstance();
+            dateTv.setText(dateFormat.format(datel));
         }
     }
 }
